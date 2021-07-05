849bb3c0b2cc3a99e816ad4a929a3253b2d17c63
nahyun574 <84331649+nahyun574@users.noreply.github.com>
2021-07-05 17:21:00 +0900

    Add files via upload

---
+++ Baekjoon/1436.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#define SIZE 1000000
+int main()
+{
+	int N;
+	int j = 0;
+	int temp = 0;
+	char name[SIZE]={"\0"};
+	scanf("%d", &N);
+	
+	for (int i = 0; i < N; i++)
+	{
+
+	}
+	
+
+	return 0;
+}
