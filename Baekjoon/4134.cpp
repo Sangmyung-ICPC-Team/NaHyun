fa296087a52e141c922bab4675087b461a3a2ecf
nahyun574 <84331649+nahyun574@users.noreply.github.com>
2021-07-05 17:21:49 +0900

    Add files via upload

---
+++ Baekjoon/4134.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#define SIZE 100
+
+int main() {
+
+	int N;
+	int number = 0;
+	int count[SIZE] = { 0 };
+	int arr[SIZE] = { 0 };
+
+	scanf("%d", &N);
+	for (int j = 0; j < N; j++) {
+		scanf("%d", &arr[j]);
+		for (int i = 0; i < arr[j]; i++) {
+			if (arr[j] % (i + 1) == 0) {
+				count[j]++;
+			}
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		if (count[i] == 2) {
+			number++;
+		}
+	}
+	return 0;
+}
